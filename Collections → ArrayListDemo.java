287a58746127c18e7ef8e03089f20b1facdc9bca
Akanksha-P1 <akankshapattanaik604@gmail.com>
2026-04-29 23:03:19 +0530

    Create Collections → ArrayListDemo.java

---
+++ Collections → ArrayListDemo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("Python");
+        list.add("C++");
+
+        System.out.println(list);
+    }
+}
